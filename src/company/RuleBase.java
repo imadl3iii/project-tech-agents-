c6ffac90f0ef4fad2ed94b3c3792c180007dc0e7
Imad Eddine HEBBACHE <imadeddine964@gmail.com>
2024-10-30 00:30:13 +0100

    first commit

---
+++ src/company/RuleBase.java
@@ -0,0 +1,15 @@
+package company;
+
+
+import java.util.ArrayList;
+
+public interface RuleBase {
+    //void setDisplay(JtextArea);
+    void trace(String text);
+    //void displayVariables(JTextArea textArea);
+    //void displayRules(JTextArea textArea);
+    void reset();
+    void backwardChain(String goalVarName);
+    void forwardChain();
+    ArrayList getGoalVariables();
+}
